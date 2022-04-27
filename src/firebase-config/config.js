96a83a53c7740910e4992925669f5e22a84eb1c8
Wanderson Marques <wanderson.marques@pixter.com.br>
2022-04-27 02:52:11 -0300

    main: firebase config

--- src/firebase-config/config.js
+++ src/firebase-config/config.js
@@ -5,24 +5,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 import 'firebase/analytics';
 
-// const app = initializeApp({
-//   apiKey: process.env.APIKEY,
-//   authDomain: process.env.AUTHDOMAIN,
-//   projectId: process.env.PROJECTID,
-//   storageBucket: process.env.STORAGEBUCKET,
-//   messagingSenderId: process.env.MESSAGINGSENDERID,
-//   appId: process.env.APPID,
-//   measurementId: process.env.MEASUREMENTID,
-// })
-
 const app = initializeApp({
-  apiKey: "AIzaSyAheyrqW1MZYq-BDj-7H8X44esBqwC1LUQ",
-  authDomain: "chat-bot-a03bd.firebaseapp.com",
-  projectId: "chat-bot-a03bd",
-  storageBucket: "chat-bot-a03bd.appspot.com",
-  messagingSenderId: "421360497197",
-  appId: "1:421360497197:web:25e886aa7aaa3efef23ea4",
-  measurementId: "G-G0ETH0KD3Z",
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID,
 })
 
 export const db = getFirestore(app)
